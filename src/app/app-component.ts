f52ac90fb39ca07719195d3bcf1469a064f67fa9
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2015-11-07 21:24:18 +0100

    Main component

--- src/app/app-component.ts
+++ src/app/app-component.ts
@@ -1,14 +1,21 @@
-import {Component, CORE_DIRECTIVES} from 'angular2/angular2';
+import {Component, provide} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {Routes, APP_ROUTES} from './route-config';
 import {HeaderComponent} from './header/header-component';
+import {FooterComponent} from './footer/footer-component';
+// import {UsersComponent} from './users/users-component';
+
 
 @Component({
 	selector: 'app',
 	templateUrl: 'app/app-component.html',
-	directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES, HeaderComponent],
-	providers: [ROUTER_PROVIDERS]
+	directives: [ROUTER_DIRECTIVES, HeaderComponent, FooterComponent],
+	providers: [
+		ROUTER_PROVIDERS,
+		provide(LocationStrategy, { useClass: HashLocationStrategy })
+	]
 })
+@RouteConfig(APP_ROUTES)
 export class AppComponent {
 	routes = Routes;
 }
