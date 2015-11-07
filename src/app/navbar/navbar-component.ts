a0d3ce88fcd7c112c963012bb0adf6d62d171aed
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2015-11-07 21:25:01 +0100

    Navbar Component

---
+++ src/app/navbar/navbar-component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'navbar-component',
+    templateUrl: './app/navbar/navbar-component.html';
+    directives: []
+})
+export class NavbarComponent {
+
+}
