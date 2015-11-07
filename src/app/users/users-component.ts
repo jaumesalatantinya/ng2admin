940f9469c472b8389ec8742eddacc2f7f6f62309
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2015-11-07 21:36:30 +0100

    Users component

---
+++ src/app/users/users-component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'users-component',
+    template: 'This should be de main view for users';
+    directives: []
+})
+export class UsersComponent {
+
+}
