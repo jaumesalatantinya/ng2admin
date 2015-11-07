cc0643f7313684d0661eb63fe65f68ab2849ea3e
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2015-11-07 21:24:45 +0100

    Footer component

---
+++ src/app/footer/footer-component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'footer-component',
+    template: 'Created by James Sala Tantinya';
+    directives: []
+})
+export class FooterComponent {
+
+}
