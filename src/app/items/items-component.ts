e67ce4635cfaaa889af5f79fc66018b6b874086e
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2015-11-07 21:36:16 +0100

    Items component

---
+++ src/app/items/items-component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'items-component',
+    template: 'This should be de main view for items';
+    directives: []
+})
+export class ItemsComponent {
+
+}
