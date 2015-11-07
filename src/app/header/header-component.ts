69a1af5d825c5c4472655fe8493e0113e98604b7
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2015-11-07 20:36:48 +0100

    Header Component

---
+++ src/app/header/header-component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'header-component',
+    templateUrl: './app/header/header-component.html';
+    directives: []
+})
+export class HeaderComponent {
+
+}
